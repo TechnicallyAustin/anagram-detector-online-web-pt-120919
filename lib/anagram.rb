d76f56d986b96c64bee06775f3963cd45d0b3bdc
Austin Johnson <technicallyaustin@gmail.com>
2020-01-21 01:04:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,18 @@
 # Your code goes here!
 class Anagram
+  attr_accessor :word 
+  def initialize(word)
+    @word = word
+  end
   
+  def self.match
+    word_arr = []
+    @word.each do |i|
+      word_arr << i.split("")
+      word_arr.each do |k|
+        
+      
+end
   
   
 end
